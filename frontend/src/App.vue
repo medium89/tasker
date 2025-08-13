a8de52af49cb31262eaabb49c0fd16be9379f91c
medium89 <ivanovdo@bk.ru>
2025-08-13 23:31:49 +0700

    feat: move navbar to sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="app">
-    <Navbar />
-    <router-view />
+    <Navbar v-if="showNavbar" />
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script setup>
 import Navbar from '@/components/Navbar.vue'
 import { auth } from '@/store/auth.js'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
 // при загрузке проверяем авторизацию
 auth.getUser()
+
+const route = useRoute()
+const showNavbar = computed(() => !['/login', '/register'].includes(route.path))
 </script>
+
+<style>
+.app {
+  display: flex;
+}
+
+.content {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+}
+</style>
